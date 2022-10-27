0f7b6ade100d2ce3adedffff66d75ef149bcdc2f
Erik Lopez <eriklopezalfaro@outlook.com>
2022-10-27 10:52:31 -0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import os
+import sys
 
 import requests
 from dotenv import load_dotenv
@@ -7,12 +8,14 @@ from dotenv import load_dotenv
 
 # API_KEY = open('api_key.txt', 'r').read()
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
-CITY = 'London'
-# API_KEY = os.getenv('api_key')
-# print(API_KEY)
+CITY = sys.argv[1] #validate
+# CITY = 'London'
+print(f'1:{CITY}, 2:{sys.argv[0]}')
 
 def configure():
     load_dotenv()
+    # API_KEY = os.getenv('api_key')
+    # print(API_KEY)
 
 def kelvin_to_celsius_fahrenheit(kelvin):
     celsius = kelvin - 273.15
@@ -23,17 +26,15 @@ def main():
     configure()
     # url = BASE_URL + 'q=' + CITY + '&APPID=dd3c88974a05a9da4c9a775c0049a127'
     url = f'{BASE_URL}q={CITY}&APPID={os.getenv("api_key")}'
-    response = requests.get(url).json()
+    response = requests.get(url).json() #make a try exept block
 
-    print(response)
+    print(f'\n{response}')
 
-    # temp_kelvin = response['main']['temp']
-    # temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
-    # print(temp_celsius, temp_fahrenheit)
-
-    # feels_like_kelvin = response['main']['feels_like']
-    # feels_c, feels_f = kelvin_to_celsius_fahrenheit(feels_like_kelvin)
-    # print(feels_c, feels_f)
+    description = response['weather'][0]['description']
+    temp_kelvin = response['main']['temp']
+    temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
+    
+    print(f'\n{CITY}: {temp_celsius}°C / {temp_fahrenheit}°F \n{description}\n')
 
     # humidity, description, sunrise
 
